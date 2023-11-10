d9884743207fcdbdd8df39d541f36a035c480be0
Mark Brady <m.brady@herts.ac.uk>
2023-11-10 13:59:03 +0000

    new file:   all.out 	modified:   checkjenky.py

--- checkjenky.py
+++ checkjenky.py
@@ -2,12 +2,12 @@
 import argparse
 import sys
 from jenkinsapi.jenkins import Jenkins
-url = "https://jenkins:8083"
+defaulturl = "https://jenkins.herts.ac.uk:8083"
 # jobs set here for testing/verification
 jobs = [
     "(vcl-la-rwa03) Import notification logs",
-    "(vcl-la-rwa03) Import AWS notification logs",
     "(hokey cokey)",
+    "(vcl-la-rwa03) Import AWS notification logs",
     "(ask) Staging wayfinding index",
 ]
 parser = argparse.ArgumentParser()
@@ -19,9 +19,9 @@ parser.add_argument(
     type=str,
 )
 parser.add_argument(
-    "-u",
     "--url",
-    help="default: " + url,
+    nargs=1,
+    help="default: " + defaulturl,
 )
 parser.add_argument(
     "--user",
@@ -31,15 +31,23 @@ parser.add_argument(
     "--password",
     help="password, default will be guest",
 )
+parser.add_argument(
+    "--list",
+    action="store_true",
+)
+parser.add_argument(
+    "--all",
+    action="store_true",
+)
 args = parser.parse_args()
 if args.jobs == None:
     pass
 else:
     jobs = args.jobs
 if args.url == None:
-    pass
+    url = defaulturl 
 else:
-    url == args.url
+    url = args.url[0]
 if args.user == None:
     user = "guest"
 if args.password== None:
@@ -48,9 +56,17 @@ if args.password== None:
 try:
     J=Jenkins(url,user,password)
 except:
-    print(f"ERROR: Could not connect to Jenkins")
+    print(f"ERROR: Could not connect to Jenkins {url}")
     sys.exit(1)
-    
+if args.list == True:
+    print('args list being used')
+    for j in J.get_jobs_list():
+        print(j)
+    sys.exit(0)
+
+if args.all == True:
+    jobs=J.get_jobs_list()
+   
 output = ""
 enabled = ""
 badresult=False
@@ -60,26 +76,28 @@ for jobname in jobs:
     if J.has_job(jobname):
         enabled =  J[jobname].is_enabled()
         if enabled == False:
-            output = output + "DISABLED " +  ' job="' + jobname + '" is_enabled=' + str(enabled)
+            print ( "DISABLED " +  ' job="' + jobname + '" is_enabled=' + str(enabled))
             disabled=True
             badresult=True
+
         else:
             status=J[jobname].get_last_build().get_status()
-            output = output + status  +  ' job="' + jobname + '" is_enabled=' + str(enabled)
-        output = output +  "\n"
+            print ( str(status) + "/status"  +  ' job="' + jobname + '" is_enabled=' + str(enabled))
     else:   
             badresult=True
             notfound=True
-            output = output + "NOTFOUND" +  ' job="' + jobname  
-            output=output+"\n"
+            print( "NOTFOUND" +  ' job="' + jobname  )
 
 if badresult == True:
     if notfound:
-        print(f"CRITICAL: NOTFOUND Job: {output}")
+        print(f"CRITICAL: Job(s) NOTFOUND returncode 2")
+        print("==============")
         sys.exit(2)
     elif disabled:
-        print(f"WARNING: DISABLED Job: {output}")
+        print(f"WARNING: Job(s) DISABLED returncode 1")
+        print("==============")
         sys.exit(1)
 else:
     print(f"OK:{output}")
     sys.exit(0)
+
