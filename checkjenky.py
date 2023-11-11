acac9d7170dc722624a736b223520956190ed9d4
Mark Brady <m.brady@herts.ac.uk>
2023-11-11 14:25:37 +0000

    modified:   checkjenky.py

--- checkjenky.py
+++ checkjenky.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 from jenkinsapi.jenkins import Jenkins
 defaulturl = "https://jenkins:8083"
+
 # jobs set here for testing/verification
 jobs = [
     "(vcl-la-rwa03) Import notification logs",
@@ -10,9 +11,9 @@ jobs = [
     "(vcl-la-rwa03) Import AWS notification logs",
     "(ask) Staging wayfinding index",
 ]
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-j",
     "--jobs",
     help='"Jenkins Job name"',
     nargs="+",
@@ -21,15 +22,15 @@ parser.add_argument(
 parser.add_argument(
     "--url",
     nargs=1,
-    help="default: " + defaulturl,
+    help="Jenkins URL (default:" + defaulturl + ")",
 )
 parser.add_argument(
     "--user",
-    help="user, default will be guest",
+    help="Jenkins user login name, (default=guest)",
 )
 parser.add_argument(
     "--password",
-    help="password, default will be guest",
+    help="password, (default=guest)",
 )
 parser.add_argument(
     "--list",
@@ -39,7 +40,7 @@ parser.add_argument(
 parser.add_argument(
     "--all",
     action="store_true",
-    help="will get all jobs from jenkins and report on them",
+    help="list and report on all jobs from jenkins",
 )
 args = parser.parse_args()
 if args.jobs == None:
@@ -49,7 +50,7 @@ else:
 if args.url == None:
     url = defaulturl 
 else:
-    url = args.url[0]
+    url = args.url[0] #one item but in a list  
 if args.user == None:
     user = "guest"
 if args.password== None:
@@ -61,14 +62,16 @@ except:
     print(f"ERROR: Could not connect to Jenkins {url}")
     sys.exit(1)
 if args.list == True:
-    print('args list being used')
     for j in J.get_jobs_list():
         print(j)
     sys.exit(0)
 
 if args.all == True:
     jobs=J.get_jobs_list()
-   
+
+# Nagios wants return codes  0(good),1(warning),2(critical)  
+# The first line is visible in thruk so we need the first line summarise
+# So that means accumulating the output
 output = ""
 enabled = ""
 badresult=False
@@ -78,28 +81,36 @@ for jobname in jobs:
     if J.has_job(jobname):
         enabled =  J[jobname].is_enabled()
         if enabled == False:
-            print ( "DISABLED " +  ' job="' + jobname + '" is_enabled=' + str(enabled))
+            output = output +  "DISABLED" +  ', job="' + jobname + \
+                '" is_enabled=' + str(enabled) + "\n"
             disabled=True
             badresult=True
 
         else:
             status=J[jobname].get_last_build().get_status()
-            print ( str(status) + "/status"  +  ' job="' + jobname + '" is_enabled=' + str(enabled))
+            if status == None:
+                if J[jobname].is_running() == True:
+                    status = "RUNNING"
+            output = output +  str(status) + ', job="' + jobname + '" is_enabled=' + str(enabled) + "\n"
     else:   
             badresult=True
             notfound=True
-            print( "NOTFOUND" +  ' job="' + jobname  )
+            output = output +  "NOTFOUND" +  ', job="' + jobname  + "\n"
 
 if badresult == True:
     if notfound:
         print(f"CRITICAL: Job(s) NOTFOUND returncode 2")
         print("==============")
+        print(output)
         sys.exit(2)
     elif disabled:
         print(f"WARNING: Job(s) DISABLED returncode 1")
         print("==============")
+        print(output)
         sys.exit(1)
 else:
-    print(f"OK:{output}")
+    print(f"OK returncode 0")
+    print("==============")
+    print(output)
     sys.exit(0)
 
